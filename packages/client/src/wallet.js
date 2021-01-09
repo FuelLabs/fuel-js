cc859b7a36d0f270fb6ae8840280268755937041
Chance Hudson <jchancehud@gmail.com>
2021-01-08 18:47:51 -0600

    use native fs for write

--- packages/client/src/wallet.js
+++ packages/client/src/wallet.js
@@ -1,4 +1,3 @@
-const write = require('write');
 const prompts = require('prompts');
 const ethers = require('ethers');
 const { sign } = require('crypto');
@@ -12,29 +11,28 @@ async function wallet(flags = {}, environment = {}) {
   try {
     const provided = flags.wallet;
     const _path = provided || defaultPath;
+    let _wallet = null;
     let json = null;
 
     // if the user provided a wallet path, try to read it
     if (provided) {
       try {
-        json = await fs.read(_path, 'utf8');
+        json = await fs.readFile(_path, 'utf8');
       } catch (readError) {
         throw new Error('Error reading wallet path: ' + readError.message);
+      }
+    }
+    // if no wallet was provided, attempt to read the default path wallet
+    if (!provided) {
+      try {
+        json = await fs.readFile(_path, 'utf8');
+      } catch (_) {
+        // ignore error
       }
     }
 
     // attempt to load, decrypto or create an Ethers wallet
     try {
-
-      // if no wallet was provided, attempt to read the default path wallet
-      if (!provided) {
-        try {
-          json = await fs.read(_path, 'utf8');
-        } catch (readDefaultError) {
-          // ignore defualt read error
-        }
-      }
-
       const message = json ?
         'Please enter your Fuel wallet decryption passphrase' :
         'Please enter a Fuel wallet encryption passphrase';
@@ -63,25 +61,28 @@ async function wallet(flags = {}, environment = {}) {
         throw new Error(error.message);
       }
 
-      // length check
+      // length check, only for new wallets
       if (!json && (password || '').length < 8) {
         throw new Error('password must be more than 8 characters');
       }
 
       // if no wallet present, attempt to load the JSON from password
       // otherwise create wallet
-      const _wallet = json ?
-        (await ethers.Wallet.fromEncryptedJson(json, password)) :
-        ethers.Wallet.createRandom();
+      if (json) {
+        _wallet = await ethers.Wallet.fromEncryptedJson(json, password);
+      } else {
+        _wallet = ethers.Wallet.createRandom();
+      }
 
-      // encrypt to wallet JSON again
-      json = await _wallet.encrypt(password);
+      // encrypt and write to file if new wallet
+      if (!json) {
+        json = await _wallet.encrypt(password);
+        await fs.writeFile(_path, json);
+      }
     } catch (readError) {
       console.error('Wallet decryption error: ' + readError.message);
+      throw new Error();
     }
-
-    // write the JSON to the appropriate path
-    await write(_path, json);
 
     // return wallet object descrypted into memory
     return _wallet;
