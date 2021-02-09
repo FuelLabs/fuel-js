b592ebfd9a5405b3feae80938acc1698694be926
SilentCicero <thenickdodson@gmail.com>
2021-02-08 21:26:48 -0500

    cleanup

--- packages/protocol2/src/test/withdraw.js
+++
@@ -1,6 +0,0 @@
-const { test, utils } = require('@fuel-js/environment');
-const withdraw = require('../withdraw');
-
-module.exports = test('withdraw', async t => {
-
-});
--- packages/protocol2/src/test/witness.js
+++
@@ -1,6 +0,0 @@
-const { test, utils } = require('@fuel-js/environment');
-const witness = require('../witness');
-
-module.exports = test('witness', async t => {
-
-});
